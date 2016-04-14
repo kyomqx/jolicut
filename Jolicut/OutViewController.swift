9a173fefefadf572e642e9bb6aa76ad8e4961ebd
Max KHAMVONGSA <khamvo_m@g03-r2p01.local>
2016-04-14 16:47:10 +0200

    max commit 2

---
+++ Jolicut/OutViewController.swift
@@ -0,0 +1,24 @@
+//
+//  OutViewController.swift
+//  Jolicut
+//
+//  Created by Max KHAMVONGSA on 14/04/16.
+//  Copyright © 2016 Max KHAMVONGSA. All rights reserved.
+//
+
+import UIKit
+
+class OutViewController: UIViewController {
+
+    @IBOutlet weak var MenuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            MenuButton.target = self.revealViewController()
+            MenuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+}
