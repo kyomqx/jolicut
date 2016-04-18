5fb69a77e805e4db0f6e840076b424669542a714
Kvin Taing <taing_k@g03-r2p03.local>
2016-04-18 11:52:00 +0200

    main storyboard

---
+++ Jolicut/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Prototypes.h
+//  Jolicut
+//
+//  Created by Kvin Taing on 18/04/16.
+//  Copyright © 2016 Max KHAMVONGSA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
