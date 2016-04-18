404d01a153a333dc6bd891b9b1e0d14890b4d574
Alexandre MISAIPHON <misaip_a@g03-r3p02.local>
2016-04-18 16:21:23 +0200

    commit201

---
+++ Jolicut/Deconnecter.swift
@@ -0,0 +1,17 @@
+//
+//  Deconnecter.swift
+//  Jolicut
+//
+//  Created by Alexandre MISAIPHON on 18/04/16.
+//  Copyright © 2016 Max KHAMVONGSA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class deco : UITableViewController {
+    
+    @IBAction func deco(sender: AnyObject) {
+        
+    }
+}
