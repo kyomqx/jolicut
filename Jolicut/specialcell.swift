0e4e215765d327f9119feda5457235182d987562
Kvin Taing <taing_k@g03-r2p03.local>
2016-04-19 14:55:54 +0200

    client coiffeur

---
+++ Jolicut/specialcell.swift
@@ -0,0 +1,30 @@
+//
+//  specialcell.swift
+//  Jolicut
+//
+//  Created by Kvin Taing on 19/04/16.
+//  Copyright © 2016 Max KHAMVONGSA. All rights reserved.
+//
+
+import UIKit
+
+class SpecialCell: UITableViewCell {
+
+    @IBOutlet var nom: UILabel!
+
+    @IBOutlet var prenom: UILabel!
+    
+    @IBOutlet var info: UILabel!
+    @IBOutlet var sexe: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
